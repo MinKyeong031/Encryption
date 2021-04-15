9a11555becdcec72b93905a11401b83b073bba4f
MinKyeong031 <s2019s07@e-mirim.hs.kr>
2021-04-15 12:21:42 +0900

    프로젝트 디자인 완성

---
+++ app/src/main/java/kr/hs/mirim/encryption/CiperActivityM.java
@@ -0,0 +1,14 @@
+package kr.hs.mirim.encryption;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CiperActivityM extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ciper_m);
+    }
+}
---
+++ app/src/main/java/kr/hs/mirim/encryption/PlainActivityP.java
@@ -0,0 +1,14 @@
+package kr.hs.mirim.encryption;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlainActivityP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plain_p);
+    }
+}
